6936a7092c89f977c931e2e4b36f5354dfd82788
Nancy Beatriz Bernal Sánchez <81131696+nancyB3a@users.noreply.github.com>
2022-11-16 10:03:53 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -6,3 +6,12 @@ from palabras import lista_palabras
 
 #2 Imprimo el Logo del Ahorcado
 print(logo)
+
+#3 inicializo mis variables
+fin_del_juego = False
+vidas = len(etapas) - 1
+para_mostrar = []
+
+#3.1 saco una palabra al azar desde la lista
+palabra_sorteada = random.choice(lista_palabras)
+largo_palabra = len(palabra_sorteada)
