600d7b013aebbcce77cf0e7416fabdc5432a6d17
Nancy Beatriz Bernal Sánchez <81131696+nancyB3a@users.noreply.github.com>
2022-11-16 09:53:02 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -1,1 +1,8 @@
 #Archivo dónde escribiremos el programa principal
+#1 Declarar librerias
+import random,os
+from monos import etapas, logo, ganador
+from palabras import lista_palabras
+
+#2 Imprimo el Logo del Ahorcado
+print(logo)
